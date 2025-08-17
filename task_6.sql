de260db96fe8f3b8b8d6bbec02cae5754bef0eca
Josphat <josphatkkioko@gmail.com>
2025-08-17 17:57:22 +0300

    updatd task 6

--- task_6.sql
+++ task_6.sql
@@ -2,10 +2,10 @@
 -- If customer_id already exists, update the row instead
 INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '124 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '124 Happiness Ave.')
-    
+
 ON DUPLICATE KEY UPDATE
     customer_name = VALUES(customer_name),
     email = VALUES(email),
